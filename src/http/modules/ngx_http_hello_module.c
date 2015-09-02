0dc9e66c0dea85cd8d58fac880832bfc4b5a97e6
unknown <zuolj@JRY-SX-024.ssscc.com.cn>
2015-09-02 14:39:29 +0800

    init commit

---
+++ src/http/modules/ngx_http_hello_module.c
@@ -0,0 +1,231 @@
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
+
+typedef struct
+{
+    ngx_str_t hello_string;
+} ngx_http_hello_loc_conf_t;
+
+static ngx_int_t ngx_http_hello_handler(ngx_http_request_t *r);
+static char *ngx_http_hello(ngx_conf_t *cf, ngx_command_t *cmd, void *conf); //函数指针
+
+static ngx_int_t ngx_http_hello_init(ngx_conf_t *cf);               //解析完http{}内的所有配置项后回调
+static ngx_int_t ngx_http_hello_preconfiguration(ngx_conf_t *cf);   //在解析http{}内的配置项前回调
+/*
+** 创建用于存储HTTP全局配置项的结构体，该结构体中的成员将保存直属于http{}块的配置项参数。
+** 它会在解析main配置项前调用
+*/
+static void *ngx_http_hello_create_main_conf(ngx_conf_t *cf);
+static char *ngx_http_hello_init_main_conf(ngx_conf_t *cf, void *conf); //解析完main配置项后回调
+/*
+** 创建用于存储可同时出现在main/srv级别配置项的结构体，该结构体中的成员与server配置是相关联的
+*/
+static void *ngx_http_hello_create_srv_conf(ngx_conf_t *cf);
+/*
+** create_srv_conf产生的结构体所要解析的配置项，可能同时出现在main/srv级别中，
+** merge_srv_conf方法可以把出现在main级别中的配置项值合并到srv级别配置项中
+*/
+static char *ngx_http_hello_merge_srv_conf(ngx_conf_t *cf, void *prev, void *conf);
+/*
+** 创建用于存储可同时出现在main、srv、loc级别的结构体，该结构体中的成员与location配置是相关联的
+*/
+static void *ngx_http_hello_create_loc_conf(ngx_conf_t *cf);
+/*
+** create_loc_conf产生的结构体所要解析的配置项，可能出现在main/srv/loc级别中，
+** merge_loc_conf方法可以分别把出现在main、srv级别的配置项合并到loc级别的配置项中
+*/
+static char *ngx_http_hello_merge_loc_conf(ngx_conf_t *cf, void *prev, void *conf);
+
+static ngx_command_t ngx_http_hello_commands[] =
+{
+    {
+        ngx_string("hello_string"),     /* 配置项名称 */
+        NGX_HTTP_MAIN_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_TAKE1,
+        ngx_http_hello,             /* 处理配置项的参数 */
+        NGX_HTTP_LOC_CONF_OFFSET,   /* 配置文件中的偏移量 */
+        offsetof(ngx_http_hello_loc_conf_t, hello_string),
+        NULL
+    },
+    ngx_null_command
+};
+
+/*
+** 8个阶段的调用顺序与定义顺序不同，
+** HTTP框架调用这些回调方法的实际顺序与nginx.conf配置项有关
+*/
+static ngx_http_module_t ngx_http_hello_module_ctx =
+{
+    ngx_http_hello_preconfiguration,        /* preconfiguration */
+    ngx_http_hello_init,                    /* postconfiguration */
+    ngx_http_hello_create_main_conf,        /* create main configuration */
+    ngx_http_hello_init_main_conf,          /* init main configuration */
+    ngx_http_hello_create_srv_conf,         /* create server configuration */
+    ngx_http_hello_merge_srv_conf,          /* merge server configuration */
+    ngx_http_hello_create_loc_conf,         /* create location configuration */
+    ngx_http_hello_merge_loc_conf,          /* merge location configuration */
+};
+
+ngx_module_t ngx_http_hello_module =
+{
+    NGX_MODULE_V1,              /* 填充处理 */
+    &ngx_http_hello_module_ctx, /* module context */
+    ngx_http_hello_commands,    /* module directives */
+    NGX_HTTP_MODULE,            /* module type */
+    NULL,                       /* init master */
+    NULL,                       /* init module */
+    NULL,                       /* init process */
+    NULL,                       /* init thread */
+    NULL,                       /* exit thread */
+    NULL,                       /* exit process */
+    NULL,                       /* exit master */
+    NGX_MODULE_V1_PADDING       /* 填充处理 */
+};
+static ngx_int_t ngx_http_hello_preconfiguration(ngx_conf_t *cf)
+{
+    ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "ngx_http_hello_preconfiguration is called");
+    return NGX_OK;
+}
+
+static ngx_int_t ngx_http_hello_init(ngx_conf_t *cf)
+{
+    ngx_http_handler_pt        *h;
+    ngx_http_core_main_conf_t  *cmcf;
+	ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "ngx_http_hello_postconfiguration is called");
+    cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
+    h = ngx_array_push(&cmcf->phases[NGX_HTTP_CONTENT_PHASE].handlers);
+    if (h == NULL)
+    {
+        return NGX_ERROR;
+    }
+    *h = ngx_http_hello_handler;
+    return NGX_OK;
+}
+
+static void *ngx_http_hello_create_main_conf(ngx_conf_t *cf)
+{
+    ngx_http_hello_loc_conf_t *main_conf = NULL;
+	ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "ngx_http_hello_create_main_conf is called");
+    main_conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_hello_loc_conf_t));
+    if (main_conf == NULL)
+    {
+        return NULL;
+    }
+    ngx_str_null(&main_conf->hello_string);
+    return main_conf;
+}
+static char *ngx_http_hello_init_main_conf(ngx_conf_t *cf, void *conf)
+{
+    ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "ngx_http_hello_init_main_conf is called");
+    return NULL;
+}
+static void *ngx_http_hello_create_srv_conf(ngx_conf_t *cf)
+{
+    ngx_http_hello_loc_conf_t *srv_conf = NULL;
+	ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "ngx_http_hello_create_srv_conf is called");
+    srv_conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_hello_loc_conf_t));
+    if (srv_conf == NULL)
+    {
+        return NULL;
+    }
+    ngx_str_null(&srv_conf->hello_string);
+    return srv_conf;
+}
+static char *ngx_http_hello_merge_srv_conf(ngx_conf_t *cf, void *prev, void *conf)
+{
+    ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "ngx_http_hello_merge_srv_conf is called");
+    return NULL;
+}
+static void *ngx_http_hello_create_loc_conf(ngx_conf_t *cf)
+{
+    ngx_http_hello_loc_conf_t *local_conf = NULL;
+    local_conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_hello_loc_conf_t));
+    if (local_conf == NULL)
+    {
+        return NULL;
+    }
+    ngx_str_null(&local_conf->hello_string);
+    ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "ngx_http_hello_create_loc_conf is called");
+    return local_conf;
+}
+static char *ngx_http_hello_merge_loc_conf(ngx_conf_t *cf, void *prev, void *conf)
+{
+    ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "ngx_http_hello_merge_loc_conf is called");
+    return NULL;
+}
+static char *ngx_http_hello(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+{
+    /*ngx_http_core_loc_conf_t *clcf;
+    clcf = ngx_http_conf_get_module_loc_conf(cf,ngx_http_core_module);
+    clcf->handler = ngx_http_hello_handler;
+    return NGX_CONF_OK;*/
+    ngx_http_hello_loc_conf_t *local_conf;
+	char *rv;
+    local_conf = conf;
+    rv = ngx_conf_set_str_slot(cf, cmd, conf);
+    ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "hello_string:%s", local_conf->hello_string.data);
+    return rv;
+}
+
+static ngx_int_t ngx_http_hello_handler(ngx_http_request_t *r)
+{
+    ngx_int_t rc;
+    ngx_buf_t *b;
+    ngx_chain_t out;
+    ngx_http_hello_loc_conf_t *my_conf;
+    u_char ngx_hello_string[1024] = {0};
+    ngx_uint_t content_length = 0;
+	ngx_str_t type = ngx_string("text/plain");
+
+    ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0, "ngx_http_hello_handler is called");
+    my_conf = ngx_http_get_module_loc_conf(r, ngx_http_hello_module);
+    ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0, "get ngx_http_hello_loc_conf_t");
+    if (my_conf == NULL)
+    {
+        ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0, "my_conf is null");
+    }
+    if (my_conf->hello_string.len == 0)
+    {
+        ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0, "empty!");
+        return NGX_DECLINED;
+    }
+    else
+    {
+        ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0, "string is not empty");
+        ngx_sprintf(ngx_hello_string, "%V", &my_conf->hello_string);
+    }
+    content_length = ngx_strlen(ngx_hello_string);
+    ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0, "els");
+    if (!(r->method & (NGX_HTTP_GET | NGX_HTTP_HEAD)))
+    {
+        return NGX_HTTP_NOT_ALLOWED;
+    }
+    rc = ngx_http_discard_request_body(r);
+    if (rc != NGX_OK)
+    {
+        return rc;
+    }
+    
+    r->headers_out.status = NGX_HTTP_OK;
+    r->headers_out.content_length_n = content_length;
+    r->headers_out.content_type = type;
+    /* HTTP框架提供的发送HTTP头部的方法 */
+    /* 返回NGX_ERROR或返回值大于0就表示不正常 */
+    rc = ngx_http_send_header(r);
+    if (rc == NGX_ERROR || rc > NGX_OK || r->header_only)
+    {
+        return rc;
+    }
+    b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
+    if (b == NULL)
+    {
+        return NGX_HTTP_INTERNAL_SERVER_ERROR;
+    }
+    b->pos = ngx_hello_string;
+    b->last = ngx_hello_string + content_length;
+    b->memory = 1;
+    b->last_buf = 1;
+    out.buf = b;
+    out.next = NULL;
+    return ngx_http_output_filter(r, &out);
+}
